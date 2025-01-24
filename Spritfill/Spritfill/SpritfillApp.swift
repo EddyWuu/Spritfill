e100f4d7d56c125608b65afe7382f673b0c649d1
Edmond Wu <edmond.wuca@gmail.com>
2025-01-23 21:54:52 -0500

    initial commit, the beginning of Spritfill

---
+++ Spritfill/Spritfill/SpritfillApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpritfillApp.swift
+//  Spritfill
+//
+//  Created by Edmond Wu on 2025-01-23.
+//
+
+import SwiftUI
+
+@main
+struct SpritfillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
