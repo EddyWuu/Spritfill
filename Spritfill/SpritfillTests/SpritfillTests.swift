07d1b2043f581ef5e24ddc9bf121269dcfe5c018
Edmond Wu <edmond.wuca@gmail.com>
2025-01-23 21:54:52 -0500

    initial commit, the beginning of Spritfill

---
+++ Spritfill/SpritfillTests/SpritfillTests.swift
@@ -0,0 +1,17 @@
+//
+//  SpritfillTests.swift
+//  SpritfillTests
+//
+//  Created by Edmond Wu on 2025-01-23.
+//
+
+import Testing
+@testable import Spritfill
+
+struct SpritfillTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
