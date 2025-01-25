cc79f0c46740d38b6b144c768fe57a9062d806d2
Edmond Wu <edmond.wuca@gmail.com>
2025-01-24 20:46:16 -0500

    initial canvas changes

---
+++ Spritfill/Spritfill/Views/SubViews/NewProjectSetUpView.swift
@@ -0,0 +1,16 @@
+//
+//  NewProjectSetUpView.swift
+//  Spritfill
+//
+//  Created by Edmond Wu on 2025-01-24.
+//
+
+import SwiftUI
+
+struct NewProjectSetUpView: View {
+    
+    var body: some View {
+    
+        Text("NewProjectSetUpView")
+    }
+}
