ad5b52af0c945e0f7bd14f7dda8420c88fbf3595
Edmond Wu <edmond.wuca@gmail.com>
2025-07-26 16:33:01 -0700

    zoom slider working

--- Spritfill/Spritfill/Views/CanvasSubViews/ToolBarView.swift
+++ Spritfill/Spritfill/Views/CanvasSubViews/ToolBarView.swift
@@ -11,28 +11,30 @@ struct ToolsBarView: View {
     @ObservedObject var toolsVM: ToolsViewModel
 
     var body: some View {
-        HStack {
-            
+        HStack(alignment: .center, spacing: 20) {
+
             VStack {
                 Text("Zoom")
                     .font(.caption)
+                    .padding(.bottom, 8)
 
                 if let canvasVM = toolsVM.canvasVM {
                     Slider(
                         value: Binding(
                             get: { canvasVM.zoomScale },
-                            set: { canvasVM.zoomScale = $0 }
+                            set: { canvasVM.zoomScale = $0.clamped(to: 0.8...5.0) }
                         ),
                         in: 0.8...5.0,
                         step: 0.1
                     )
+                    .frame(width: 200)
                     .rotationEffect(.degrees(-90))
-                    .frame(height: 120)
+                    .scaleEffect(x: 0.5, y: 0.5)
+                    .frame(height: 100)
                 }
             }
+            .frame(width: 50)
             .padding(.leading)
-            
-            Spacer()
 
             HStack(spacing: 20) {
                 ForEach(toolsVM.availableTools, id: \.self) { tool in
@@ -48,4 +50,3 @@ struct ToolsBarView: View {
         .frame(maxWidth: .infinity)
     }
 }
-
